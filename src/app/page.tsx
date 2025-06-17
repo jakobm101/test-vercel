7a1f62a5c3de802d98d9f2bc19e87dd1e0f2bdb4
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-17 18:26:41 +0200

    link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
       <Button>Click me</Button>
       <Button variant="outline">Button</Button>
       <Button asChild>
-        <Link href="/login">Login</Link>
+        <Link href="#">Login</Link>
       </Button>
       <Button variant="destructive">Destructive</Button>
       <Button variant="secondary" size="icon" className="size-8">
@@ -80,7 +80,7 @@ export default function Home() {
               <ul className="grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                 <li className="row-span-3">
                   <NavigationMenuLink asChild>
-                    <a
+                    <Link
                       className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-linear-to-b p-6 no-underline outline-hidden select-none focus:shadow-md"
                       href="/"
                     >
@@ -90,7 +90,7 @@ export default function Home() {
                       <p className="text-muted-foreground text-sm leading-tight">
                         Beautifully designed components built with Tailwind CSS.
                       </p>
-                    </a>
+                    </Link>
                   </NavigationMenuLink>
                 </li>
                 <ListItem href="/docs" title="Introduction">
